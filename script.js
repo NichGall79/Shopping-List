3af0525991acbefe1a1293b50c137742d202c998
Nicholas Gallegos <ngallegos61@cnm.edugit config --global pull.rebase truegit config --global user.email ngallegos61@cnm.edu>
2020-03-04 16:44:41 -0600

    created a new project

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function addItemToShoppingList() {
 		});
 	}
 
-function createListItemHtml(itemName, itemAmount) {
+function createListItemHtml(itemName, itemAmount, id) {
 	return `<li id="item${id}">
 				${itemName} - ${itemAmount}
 			<button id="button${id}" type="button">Delete Item</button>
